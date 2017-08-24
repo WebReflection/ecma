7c9e67687877b497f58a658431fe7e2d1626ede4
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-08-24 15:05:21 +0100

    using innerHTML to test IE6+ too

--- bundle.js
+++ bundle.js
@@ -2,5 +2,5 @@ window.WAS_AFTER = window.AFTER;
 
 setTimeout(function () {
   document.documentElement.className = window.WAS_AFTER ? 'ok' : 'error';
-  document.body.textContent = 'ES5';
+  document.body.innerHTML = 'ES5';
 }, 1000);
